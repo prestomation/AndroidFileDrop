15531183c5404a7a97386110843fc5a574ecbe17
Preston Tamkin <prestomation@prestomation-eeepc.(none)>
2011-06-27 17:36:21 -0400

    Fixed file uploading Added upload/geturl

--- pyappengine/androidfiledrop/urls.py
+++ pyappengine/androidfiledrop/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('androidfiledrop.views',
     ('api/devices$', 'devices'),
     ('^api/files$', 'files'),
+    ('^api/upload/url$', 'uploadurl'),
     ('^api/upload$', 'upload'),
     ('^api/notify$', 'notify'),
     ('^api/notify/(?P<devname>.+)$', 'notify'),
--- test/devservertest.py
+++ test/devservertest.py
@@ -1,7 +1,7 @@
 import authlib
 import sys
 
-DEV_ID = "njYKFhYMYQWVZgKMypgalUGXYZjWa2ddhZCiocsqKe2PtXxBoM3y1SsSxrZbEy4UQ3nOnZs7UlkNMZ5DkrW5CHMBTtndvtHWN_xB4osm3hxJHevPWHBoO48KXZ7bu8HowxXG2aY8tp3rheOAJEladhFwQjs8wiGwmqLhwbDfsRfsZZoTSrOqErocTeOvseH7nxOXCByQ_WN0OkQDtkTg6Obz5plAXMxoimrK4UwO"
+DEV_ID = "njfdsfdsYQWVZgKMypgalUGXYZjWa2ddhZCiocsqKe2PtXxBoM3y1SsSxrZbEy4UQ3nOnZs7UlkNMZ5DkrW5CHMBTtndvtHWN_xB4osm3hxJHevPWHBoO48KXZ7bu8HowxXG2aY8tp3rheOAJEladhFwQjs8wiGwmqLhwbDfsRfsZZoTSrOqErocTeOvseH7nxOXCByQ_WN0OkQDtkTg6Obz5plAXMxoimrK4UwO"
 afdConn = authlib.AppEngineClient("androidfiledrop", "prestomation@gmail.com", sys.argv[1], True)
 print afdConn.registerDevice(DEV_ID, "Evo").read()
 #print afdConn.getDevices().read()
